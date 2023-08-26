d2365d091048b53997d125d29452ad6d6a0b89e1
Zach Bernal <chrisknyfe@gmail.com>
2023-08-25 18:05:45 -0700

    adapt ocean floor to camera.far and the ocean mesh's edge.

---
+++ example/ck_water_shader/water_shader_examples.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+## You can update your custom nodes' LOD settings from a WaterMaterialDesigner
+func _on_deep_ocean_designer_update_lod(far_distance, middle_distance, unit_size):
+	$Ocean/OceanFloor.scale = Vector3(far_distance * 2.0, 1.0, far_distance * 2.0)
+	$Ocean/OceanFloor.drop_far = far_distance * 0.75
