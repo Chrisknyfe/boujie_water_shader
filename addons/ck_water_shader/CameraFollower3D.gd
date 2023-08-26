f31e0ef1d884dbdc7690291fdc1f2f7a394bdc78
Zach Bernal <chrisknyfe@gmail.com>
2023-08-25 18:23:48 -0700

    Docs

--- addons/ck_water_shader/CameraFollower3D.gd
+++ addons/ck_water_shader/CameraFollower3D.gd
@@ -1,15 +1,24 @@
 extends Node3D
 class_name CameraFollower3D
 
-@export_node_path("Node3D") var victim_path
+## CameraFollower3D pushes the currently active camera's position onto a target path.
+## It can independently sync the X, Y and Z coordinate of the camera and target.
+## It can snap the target's position to multiples of a given unit size.
+
+## Target which will be repositioned by this node
+@export_node_path("Node3D") var target_path
+## Enable or disable this node
 @export var enable: bool = true
 
 const X_AXIS = 1
 const Y_AXIS = 2
 const Z_AXIS = 4
+## Select which components of the target's position will be changed.
 @export_flags("x", "y", "z") var follow_axes = 5
 
+## Enable or disable snapping the target's position
 @export var snap: bool = false
+## Snapping unit size. The target's position will be set to a multiple of this unit size.
 @export var snap_unit: float = 10.0
 
 func do_snap(x: float):
@@ -20,11 +29,11 @@ func do_snap(x: float):
 func _process(delta):
 	if not Engine.is_editor_hint() and enable:
 		var camera = get_viewport().get_camera_3d()
-		var v: Node3D = get_node(victim_path)
+		var t: Node3D = get_node(target_path)
 		if follow_axes & X_AXIS:
-			v.global_position.x = do_snap(camera.global_position.x)
+			t.global_position.x = do_snap(camera.global_position.x)
 		if follow_axes & Y_AXIS:
-			v.global_position.y = do_snap(camera.global_position.y)
+			t.global_position.y = do_snap(camera.global_position.y)
 		if follow_axes & Z_AXIS:
-			v.global_position.z = do_snap(camera.global_position.z)
+			t.global_position.z = do_snap(camera.global_position.z)
 
