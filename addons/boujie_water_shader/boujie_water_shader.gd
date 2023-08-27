44137e36b4d3144144f9e81d779adb3ec8c0bef8
Chrisknyfe <chrisknyfe@gmail.com>
2023-08-27 16:47:33 -0700

    Moved types to a subfolder; organized wave params; comments

--- addons/boujie_water_shader/boujie_water_shader.gd
+++ addons/boujie_water_shader/boujie_water_shader.gd
@@ -3,9 +3,9 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("CameraFollower3D", "Node3D", preload("res://addons/boujie_water_shader/CameraFollower3D.gd"), preload("res://addons/boujie_water_shader/icons/CameraFollower3D.svg"))
-	add_custom_type("Ocean", "Node3D", preload("res://addons/boujie_water_shader/Ocean.gd"), preload("res://addons/boujie_water_shader/icons/Ocean.svg"))
-	add_custom_type("WaterMaterialDesigner", "Node", preload("res://addons/boujie_water_shader/WaterMaterialDesigner.gd"), preload("res://addons/boujie_water_shader/icons/WaterMaterialDesigner.svg"))
+	add_custom_type("CameraFollower3D", "Node3D", preload("res://addons/boujie_water_shader/types/CameraFollower3D.gd"), preload("res://addons/boujie_water_shader/icons/CameraFollower3D.svg"))
+	add_custom_type("Ocean", "Node3D", preload("res://addons/boujie_water_shader/types/Ocean.gd"), preload("res://addons/boujie_water_shader/icons/Ocean.svg"))
+	add_custom_type("WaterMaterialDesigner", "Node", preload("res://addons/boujie_water_shader/types/WaterMaterialDesigner.gd"), preload("res://addons/boujie_water_shader/icons/WaterMaterialDesigner.svg"))
 
 
 func _exit_tree():
