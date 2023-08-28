6e6500536575bc347efb865ae79bfb4c5d0f1136
Chrisknyfe <chrisknyfe@gmail.com>
2023-08-27 19:59:23 -0700

    fix warnings

--- example/boujie_water_shader/water_shader_examples.gd
+++ example/boujie_water_shader/water_shader_examples.gd
@@ -6,6 +6,6 @@ extends Node3D
 @onready var ocean_floor = $DeepOcean/OceanFloor
 
 ## You can update your custom nodes' LOD settings from a WaterMaterialDesigner
-func _on_water_material_designer_update_lod(far_distance, middle_distance, unit_size):
+func _on_water_material_designer_update_lod(far_distance, _middle_distance, _unit_size):
 	ocean_floor.scale = Vector3(far_distance * 2.0, 1.0, far_distance * 2.0)
 	ocean_floor.drop_far = far_distance * 0.75
