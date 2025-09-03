f873b9aebc69a50f65c8fadf2ff5ab57ca448835
Zach Bernal <chrisknyfe@gmail.com>
2025-09-02 20:53:39 -0700

    option to use external time in water shader

--- addons/boujie_water_shader/prefabs/ocean_prefab.gd
+++ addons/boujie_water_shader/prefabs/ocean_prefab.gd
@@ -1,6 +1,8 @@
 @tool
 extends Ocean
 
+var shader_time: float = 0.0
+
 ## Emitted when level-of-detail settings are updated.
 ## This signal may fire in the editor. Make sure any connected scripts are also tool scripts.
 signal updated_lod(far_distance: float, middle_distance: float, unit_size: float)
@@ -8,3 +10,9 @@ signal updated_lod(far_distance: float, middle_distance: float, unit_size: float
 
 func _on_water_material_designer_updated_lod(far_distance, middle_distance, unit_size):
 	updated_lod.emit(far_distance, middle_distance, unit_size)
+
+func _process(delta):
+	if shader_time == null:
+		shader_time = 0.0
+	shader_time += delta
+	material.set_shader_parameter("external_time", shader_time)
